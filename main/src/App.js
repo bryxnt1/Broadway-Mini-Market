35b3df88aff1eb83e5411c7901e9b5b0af8e2445
Bryxnt1 <bryantperez56@gmail.com>
2023-09-08 18:58:50 -0400

    components and pages

--- main/src/App.js
+++ main/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Navbar from './components/navbar';
-import {} from "react-router-dom"
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Home from './pages/Home';
 
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar />
+      <Switch>
+        <Route path='/' exact component={{}}/>
+      </Switch>
+      </Router>
+      
     </div>
   );
 }
